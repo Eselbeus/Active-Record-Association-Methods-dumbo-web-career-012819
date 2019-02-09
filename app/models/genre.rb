b117eafc37db2a7a1ecac8f4edc62ab5b271324c
eselbeus <eselbeus@gmail.com>
2019-02-09 21:25:31 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,7 +16,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.song.map do |song|
+    self.songs.map do |song|
       song.artist
     end
   end
